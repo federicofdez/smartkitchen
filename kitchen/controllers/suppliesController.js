88c6618055322dd15d785b18f51982ae87247fac
Federico Fernández <fafermor@gmail.com>
2018-01-09 10:42:57 +0100

    Add Full Home View and All Supplies View

---
+++ kitchen/controllers/suppliesController.js
@@ -0,0 +1,6 @@
+// GET /supplies
+exports.index = function(req, res, next){
+
+	res.render('supplies/index');
+
+};
--- kitchen/routes/index.js
+++ kitchen/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var suppliesController = require('../controllers/suppliesController')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/supplies', suppliesController.index);
 
 module.exports = router;
